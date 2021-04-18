0e63623a517a5da3d6d763d95edaf6ad79040b7a
Powafu <72950006+Powafu@users.noreply.github.com>
2021-04-18 01:49:39 -0500

    ENUMS

--- Powagressive/src/Powagressive.kt
+++ Powagressive/src/Powagressive.kt
@@ -25,31 +25,33 @@ class Powagressive : Script(), Painting {
     private var rand = (25..199).random().toLong()
     private var fLvl = Skills.getActualLevel(SKILLS.FLETCHING)
 
-    private var log = arrayOf(
-            "Logs",
-            "Longbow (u)",
-            "Oak shortbow (u)",
-            "Oak longbow (u)",
-            "Willow shortbow (u)",
-            "Willow longbow (u)",
-            "Maple shortbow (u)",
-            "Maple longbow (u)",
-            "Yew longbow (u)",
-            "Magic longbow (u)"
-    )
+    enum class Bow(private val lvl: Int, val material: String, val product: String) {
+        LOGS(1,  "Logs", "arrow shafts"),
+        LONGBOW(10,  "Longbow (u)", "Longbow"),
+        OAK_SHORT(20,  "Oak shortbow (u)", "Oak shortbow"),
+        OAK_LONG(25,  "Oak longbow (u)", "Oak longbow"),
+        WILLOW_SHORT(35, "Willow shortbow (u)", "Willow shortbow"),
+        WILLOW_LONG(40, "Willow longbow (u)", "Willow longbow"),
+        MAPLE_SHORT(50, "Maple shortbow (u)", "Maple shortbow"),
+        MAPLE_LONG(55,  "Maple longbow (u)", "Maple longbow"),
+        YEW_LONG(70, "Yew longbow (u)", "Yew longbow"),
+        MAGIC_LONG(85, "Magic longbow (u)", "Magic longbow");
 
-    private var bowToFletch = arrayListOf(
-            "arrow shafts",
-            "Longbow",
-            "Oak shortbow",
-            "Oak longbow",
-            "Willow shortbow",
-            "Willow longbow",
-            "Maple shortbow",
-            "Maple longbow",
-            "Yew longbow",
-            "Magic longbow"
-    )
+        private fun canString(): Boolean {
+            return Skills.getActualLevel(SKILLS.FLETCHING) >= lvl
+        }
+
+        companion object {
+            val targetBow: Bow
+                get() {
+                    var target = LOGS
+                    for (b in Powagressive.Bow.values()) {
+                        if (b.canString() && b.lvl > target.lvl) target = b
+                    }
+                    return target
+                }
+        }
+    }
 
     override fun onPaint(g: Graphics) {
         val timeRan = System.currentTimeMillis() - startTime
@@ -65,7 +67,7 @@ class Powagressive : Script(), Painting {
         g.font = Font("Calibri", Font.BOLD, 16)
         g.color = Color.yellow
         g.drawString("| Bows Fletched: $amountCut", 280, 315)
-        g.drawString("| Current Cut: " + bowType(), 10, 315)
+        g.drawString("| Current Cut: " + Bow.targetBow.product, 10, 315)
         g.drawString("| Fletching Level: " + SKILLS.FLETCHING.actualLevel + " ("
                 + (SKILLS.FLETCHING.actualLevel - fletchingLevelBefore) + ")", 10, 300)
         g.drawString("| Experience Gained: " + gainedXp(), 280, 300)
@@ -83,10 +85,10 @@ class Powagressive : Script(), Painting {
 
     private fun getTotalFletched() {
         if (logCounter == 0) {
-            logCounter = Inventory.getCount(logType())
-        } else if (Inventory.getCount(logType()) < logCounter) {
+            logCounter = Inventory.getCount(Bow.targetBow.material)
+        } else if (Inventory.getCount(Bow.targetBow.material) < logCounter) {
             amountCut += 1
-            logCounter = Inventory.getCount(logType())
+            logCounter = Inventory.getCount(Bow.targetBow.material)
         }
 
     }
@@ -106,38 +108,6 @@ class Powagressive : Script(), Painting {
         return SKILLS.FLETCHING.xp - fletchingExpBefore
     }
 
-    private fun bowType(): String {
-        return when (fLvl) {
-            in 0..9 -> bowToFletch[0]
-            in 10..19 -> bowToFletch[1]
-            in 20..24 -> bowToFletch[2]
-            in 25..34 -> bowToFletch[3]
-            in 35..40 -> bowToFletch[4]
-            in 40..49 -> bowToFletch[5]
-            in 50..54 -> bowToFletch[6]
-            in 55..69 -> bowToFletch[7]
-            in 70..84 -> bowToFletch[8]
-            in 85..99 -> bowToFletch[9]
-            else -> "You're not in runescape anymore"
-        }
-    }
-
-    private fun logType(): String {
-        return when (fLvl) {
-            in 0..9 -> log[0]
-            in 10..19 -> log[1]
-            in 20..24 -> log[2]
-            in 25..34 -> log[3]
-            in 35..40 -> log[4]
-            in 40..49 -> log[5]
-            in 50..54 -> log[6]
-            in 55..69 -> log[7]
-            in 70..84 -> log[8]
-            in 85..99 -> log[9]
-            else -> "Kansas"
-        }
-    }
-
     private fun bankProcess() {
         if (Banking.isInBank()) {
             if (!Banking.isBankLoaded()) {
@@ -145,12 +115,12 @@ class Powagressive : Script(), Painting {
             }
             Banking.depositAll()
             if (gainedLevel() < 10) {
-                if (Inventory.getCount(logType()) < 1 && Banking.find(logType()).isNotEmpty()) { Banking.withdraw(27, logType()) }
+                if (Inventory.getCount(Bow.targetBow.material) < 1 && Banking.find(Bow.targetBow.material).isNotEmpty()) { Banking.withdraw(27, Bow.targetBow.material) }
                 if (Inventory.getCount(knife) < 1 && Banking.find(knife).isNotEmpty()) { Banking.withdraw(1, knife) }
                 else errorMessage()
             }
             if (gainedLevel() >= 10) {
-                if (Inventory.getCount(logType()) < 1 && Banking.find(logType()).isNotEmpty()) { Banking.withdraw(14, logType()) }
+                if (Inventory.getCount(Bow.targetBow.material) < 1 && Banking.find(Bow.targetBow.material).isNotEmpty()) { Banking.withdraw(14, Bow.targetBow.material) }
                 if (Inventory.getCount(Bs) < 1 && Banking.find(Bs).isNotEmpty()) { Banking.withdraw(14, Bs) }
                 else errorMessage()
             }
@@ -158,36 +128,27 @@ class Powagressive : Script(), Painting {
     }
 
     private fun whenCut() {
-        val myKnifeCount = Inventory.getCount(knife)
-        val myLogTypeCount = Inventory.getCount(logType())
-        val myBsCount = Inventory.getCount(Bs)
-
         when {
-            gainedLevel() < 10 && myKnifeCount >= 1 && myLogTypeCount >= 1 -> howCut()
-            gainedLevel() >= 10 && myBsCount >= 1 && myLogTypeCount >= 1 -> howCut()
+            gainedLevel() < 10 && Inventory.getCount(knife) >= 1 && Inventory.getCount(Bow.targetBow.material) >= 1 -> howCut()
+            gainedLevel() >= 10 && Inventory.getCount(Bs) >= 1 && Inventory.getCount(Bow.targetBow.material) >= 1 -> howCut()
             else -> bankProcess()
         }
     }
 
     private fun howCut() {
-        val myKnife = Inventory.find(knife)
-        val myBs = Inventory.find(Bs)
-        val myLogType = Inventory.find(logType())
-
         while (!Interfaces.isInterfaceSubstantiated(270)) {
-            if (gainedLevel() < 10 && myKnife.isNotEmpty() && myLogType.isNotEmpty()) { myKnife[0].click() && myLogType[0].click() }
-            if (gainedLevel() >= 10 && myBs.isNotEmpty() && myLogType.isNotEmpty()) { myBs[0].click() && myLogType[0].click() }
+            if (gainedLevel() < 10 && Inventory.find(knife).isNotEmpty() && Inventory.find(Bow.targetBow.material).isNotEmpty()) { Inventory.find(knife)[0].click() && Inventory.find(Bow.targetBow.material)[0].click() }
+            if (gainedLevel() >= 10 && Inventory.find(Bs).isNotEmpty() && Inventory.find(Bow.targetBow.material).isNotEmpty()) { Inventory.find(Bs)[0].click() && Inventory.find(Bow.targetBow.material)[0].click() }
             sleep(1000,1600)
         }
 
         if (Timing.waitCondition({ Interfaces.isInterfaceSubstantiated(270) }, Random.nextLong(400, 600))) { Keyboard.sendPress(' ', 32) }
 
-        while (Inventory.getCount(logType()) > 0) {
+        while (Inventory.getCount(Bow.targetBow.material) > 0) {
             when {
-                Inventory.getCount(logType()) == 0 -> break
+                Inventory.getCount(Bow.targetBow.material) == 0 -> break
                 gainedLevel() < 10 && Inventory.getCount(knife) == 0 -> break
                 Inventory.getCount(Bs) == 0 -> break
-                !Inventory.open() -> break
                 Interfaces.isInterfaceSubstantiated(233) -> break
                 else -> sleep(rand)
             }
@@ -215,7 +176,7 @@ class Powagressive : Script(), Painting {
             shouldRun = if (!Banking.isInBank()) {
                 println("Player not in Bank")
                 false
-            } else if (Inventory.getCount(Powagressive().logType()) < 1) {
+            } else if (Inventory.getCount(Bow.targetBow.material) < 1) {
                 println("No logs to fletch. Stopping script.")
                 false
             } else if (Inventory.getCount(Powagressive().knife) < 1) {
